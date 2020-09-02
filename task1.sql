d8531cb3c426814391d02175aee425d18132a8f3
Andre <upgoto@gmail.com>
2020-09-02 23:39:17 +0500

    L2

--- task1.sql
+++ task1.sql
@@ -1,3 +1,6 @@
+/*
+Создайте базу данных example, разместите в ней таблицу users, состоящую из двух столбцов, числового id и строкового name.
+*/
 create database if not exists example;
 use example;
 create table users (id int, name varchar(50));
